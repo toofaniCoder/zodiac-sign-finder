93d4f4697eb8e35c8e6c600e78d6355c735db3e5
toofaniCoder <smartcoder500@gmail.com>
2021-05-07 10:43:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Zodiac Signs (12)
+
+```
+Aquarius January 20th — February 18th
+Pisces February 19th — March 20th
+Aries March 21 — April 19th
+Taurus April 20th — May 20th
+Gemini May 21 — June 20th
+Cancer June 21 —July 22
+Leo July 23rd — August 22
+Virgo August 23 — September 22nd
+Libra September 23 — October 22
+Scorpio October 23rd — November 21
+Sagittarius November 22 — December 21
+Capricorn December 22nd — January 19th
+```
---
+++ js/result.js
@@ -0,0 +1,13 @@
+let zodiacSign = sessionStorage.getItem("zodiacSign");
+let zodiacDate = sessionStorage.getItem("zodiacDate");
+
+if (!zodiacSign) {
+  location.href = "./index.html";
+}
+document.title = `Your Zodiac - ${zodiacSign}`;
+
+const imageElement = document.querySelector("img");
+imageElement.src = `./icons/${zodiacSign}.svg`;
+imageElement.alt = zodiacSign;
+document.getElementById("zodiac-sign").innerHTML = zodiacSign;
+document.getElementById("zodiac-date").innerHTML = zodiacDate;
